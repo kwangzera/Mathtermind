1d27de31b1e018040de9a57e49f0c2fc90f712fe
Togohogo1 <59260907+Togohogo1@users.noreply.github.com>
2021-05-17 21:47:25 -0400

    refactored help messages should be complete (but not formatted)

--- cogs/gamemodes.py
+++ cogs/gamemodes.py
@@ -20,13 +20,40 @@ class Gamemodes(commands.Cog):
 
     @commands.command(aliases=["cl"])
     async def classic(self, ctx):
-        """Starts a Mathtermind game in classic mode"""
+        """Starts a Mathtermind game in classic mode
+
+        A winning combination of 3 unique numbers is randomly selected and the user's goal is to guess those numbers. The user
+        is given 7 guesses to try to figure out what the winning combination is and an 8th to determine it.
+
+        For this gamemode, the user is allowed to guess from a minimum
+        of 1 up to a maximum of 4 unique numbers each turn. After each guess the game will respond with the number of
+        numbers in the winning combination that match those in the user's guess.
+
+        On the final guess, the user is required to guess 3 unique numbers. If all 3 of the user's numbers
+        (in any particular order) match the winning combination, then the user wins. Otherwise, the user loses.
+        
+        Enter ;classic to start the game or ;help guess for more detailed information about guessing.
+        """
 
         await self.create_game(ctx, Classic(ctx))
 
     @commands.command(aliases=["rp"])
     async def repeat(self, ctx):
-        """Starts a Mathtermind game in repeat mode"""
+        """Starts a Mathtermind game in repeat mode
+
+        A winning combination of 3 numbers (not necessarily unique) is randomly selected and the user's goal is to
+        guess those numbers. The user is given 7 guesses to try to figure out what the winning combination is and an 8th
+        to determine it.
+
+        For this gamemode, the user is allowed to guess from a minimum
+        of 1 up to a maximum of 4 numbers (remember, they don't have to be unique) each turn. After each guess the
+        game will respond with the number of numbers in the winning combination that match those in the user's guess.
+
+        On the final guess, the user is required to guess 3 unique numbers. If all 3 of the user's numbers
+        (in any particular order) match the winning combination, then the use wins. Otherwise, the user loses.
+        
+        Enter ;repeat to start the game in ;help guess for more detailed information about guessing.
+        """
 
         await self.create_game(ctx, Repeat(ctx))
 
@@ -34,8 +61,24 @@ class Gamemodes(commands.Cog):
     async def detective(self, ctx):
         """Starts a Mathtermind game in detective mode
 
+        A winning combination of 3 unique numbers is randomly selected and the user's goal is to guess those numbers. The user
+        is given 8 guesses to try to figure out what the winning combination is and a 9th to determine it. However, 
+        with an extra guess comes with a twist.
 
+        For this gamemode, the user is allowed to guess from a minimum
+        of 1 up to a maximum of 4 unique numbers each turn. After each guess the game will tell the user the number of
+        numbers in the winning combination that match those in the user's guess.
+
+        In one of the first 4 guesses, the game will deliberately try to trick the user by returning an
+        incorrect number of matches. However, the game gives the user a chance to try to identify which guess had an incorrect
+        number of matches (for more details enter ;help identify)
+
+        On the final guess, the user is required to guess 3 unique numbers. If all 3 of the user's numbers
+        (in any particular order) match the winning combination, then the user wins. Otherwise, the user loses.
+        
+        Enter ;detective to start the game or ;help guess for more detailed information about guessing.
         """
+
         await self.create_game(ctx, Detective(ctx))
 
     async def create_game(self, ctx, gametype):
